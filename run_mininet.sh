d30502d2d512693777c73f07e089de24f74ba45b
maxogod <maxi18wars@gmail.com>
2025-04-13 22:56:59 -0300

    ops: add run mininet script + example topology

--- run_mininet.sh
+++ run_mininet.sh
@@ -3,13 +3,14 @@
 IMAGE_NAME="mininet_img"
 
 if [ "$1" == "build" ]; then
-    docker build -t $IMAGE_NAME -f ./Dockerfile.mininet .
+    docker build -t $IMAGE_NAME -f ./mininet/Dockerfile.mininet ./mininet/
 elif [ "$1" == "run" ]; then
     shift
     docker run -it --rm --privileged --net=host \
         -e DISPLAY \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v /lib/modules:/lib/modules \
+        -v $(pwd):/root \
         $IMAGE_NAME "$@"
 else
     echo "Usage: $0 {build|run} [docker_run_args...]"
