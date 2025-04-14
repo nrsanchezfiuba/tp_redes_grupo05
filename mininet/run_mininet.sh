c7be66bfe895416743dddf677c35bf2205f4cbb0
maxogod <maxi18wars@gmail.com>
2025-04-13 22:32:43 -0300

    ops: Add mininet dockerfile

---
+++ mininet/run_mininet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IMAGE_NAME="mininet_img"
+
+if [ "$1" == "build" ]; then
+    docker build -t $IMAGE_NAME -f ./Dockerfile.mininet .
+elif [ "$1" == "run" ]; then
+    shift
+    docker run -it --rm --privileged --net=host \
+        -e DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v /lib/modules:/lib/modules \
+        $IMAGE_NAME "$@"
+else
+    echo "Usage: $0 {build|run} [docker_run_args...]"
+    exit 1
+fi
+
