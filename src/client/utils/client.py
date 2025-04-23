aa5eb57a67004df6f48ecd8e2b93882f7f5d8c54
sev <santisevitz@hotmail.com>
2025-04-23 13:40:59 -0300

    seperating client protocol to another file

--- src/client/utils/client.py
+++ src/client/utils/client.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
 from argparse import Namespace
-from typing import Tuple
+
+from client.utils.client_protocol import ClientProtocol
 
 
 class Client:
@@ -66,14 +67,3 @@ class Client:
             print("Starting client...")
 
         asyncio.run(self.start_client())
-
-
-class ClientProtocol(asyncio.DatagramProtocol):
-    def __init__(self) -> None:
-        self.queue: asyncio.Queue[Tuple[bytes, Tuple[str, int]]] = asyncio.Queue()
-
-    def datagram_received(self, data: bytes, addr: Tuple[str, int]) -> None:
-        self.queue.put_nowait((data, addr))
-
-    async def recv(self) -> Tuple[bytes, Tuple[str, int]]:
-        return await self.queue.get()
