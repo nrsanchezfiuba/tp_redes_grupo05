fc202107d14254d4dc8ea06a88dc28800db7d5fe
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-04-26 18:02:45 -0300

    feat: ConnectionSocket for client and server

--- src/common/socket/connection_socket.py
+++ src/common/socket/connection_socket.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Tuple
+from typing import Optional, Tuple
 
 from common.socket.packet import Packet
 from common.socket.udp_socket import UDPSocket
@@ -7,16 +7,33 @@ from common.socket.udp_socket import UDPSocket
 
 class ConnectionSocket:
 
-    def __init__(self, host: str, port: int, queue: asyncio.Queue[Packet]):
+    @classmethod
+    def for_client(cls, host: str, port: int) -> "ConnectionSocket":
+        return cls(host, port, None)
+
+    @classmethod
+    def for_server(
+        cls, host: str, port: int, queue: asyncio.Queue[Packet]
+    ) -> "ConnectionSocket":
+        return cls(host, port, queue)
+
+    def __init__(self, host: str, port: int, queue: Optional[asyncio.Queue[Packet]]):
         self.addr: Tuple[str, int] = (host, port)
         self.udp_socket: UDPSocket = UDPSocket()
-        self.queue: asyncio.Queue[Packet] = queue
+        self.queue: Optional[asyncio.Queue[Packet]] = queue
 
     async def send(self, packet: Packet) -> None:
         await self.udp_socket.send_all(packet.to_bytes(), self.addr)
 
     async def recv(self) -> Packet:
-        recv_pkt = await self.queue.get()
+        recv_pkt = None
+
+        if not self.queue:
+            response, _ = await self.udp_socket.recv_all()
+            recv_pkt = Packet.from_bytes(response)
+        else:
+            recv_pkt = await self.queue.get()
+
         return recv_pkt
 
     async def close(self) -> None:
