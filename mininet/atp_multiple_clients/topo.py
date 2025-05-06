8965d6c50e836d1c4693984bfdc1103ef3c40f13
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-05-06 14:58:20 -0300

    feat: give up xterm and create files and dirs auto

--- mininet/atp_multiple_clients/topo.py
+++ mininet/atp_multiple_clients/topo.py
@@ -1,6 +1,5 @@
 import json
 import os
-import random
 import sys
 import time
 from typing import Any, Dict
@@ -31,27 +30,36 @@ class ClientServerTopo(Topo):  # type: ignore
             raise ValueError(
                 "Loss percentage and clients must be properly defined in the configuration."
             )
-        loss = self.loss / len(self.clients)
+        loss = self.loss / 2
 
         # Create server (h1) and switch (s1)
         server_ip = "10.0.0.1"
         server = self.addHost("h1", ip=server_ip)
         s1 = self.addSwitch("s1")
         self.addLink(server, s1, cls=TCLink, loss=loss)
-        server_command = f"PYTHONPATH=src python3 ./src/start_server.py -v -H {server_ip} -p {SERVER_PORT} -s {self.server_storage_path} -r {self.protocol} --log-file mn_server.log"
-        self.actions[server] = server_command
 
-        # Create clients based on config
-        if not isinstance(self.clients, list):
-            raise ValueError("Clients must be a list.")
-
-        for i, client_config in enumerate(self.clients, start=2):
+        for i, _ in enumerate(self.clients, start=2):
             client_name = f"h{i}"
             client_host = self.addHost(client_name)
             self.addLink(client_host, s1, cls=TCLink, loss=loss)
 
-            client_command = f"PYTHONPATH=src python3 ./src/{client_config['action']}.py -v -H {server_ip} -p {SERVER_PORT} -n {client_config['file']} -d {client_config['path']} -r {self.protocol} --log-file mn_client{i-1}.log && echo client {i-1} done"
-            self.actions[client_host] = client_command
+
+def create_file(file_path: str, size_kb: int = 10) -> None:
+    chunk = os.urandom(1024)
+
+    with open(file_path, "ab") as f:
+        for _ in range(size_kb):
+            f.write(chunk)
+
+    print(f"Created file {file_path} ({size_kb}KB)")
+
+
+def create_dirs_and_files(dirpath: str, filename: str, file_size_kb: int = 10) -> None:
+    os.makedirs(dirpath, exist_ok=True)
+    print(f"Created client directory: {dirpath}")
+
+    file_path = os.path.join(dirpath, filename)
+    create_file(file_path, file_size_kb)
 
 
 def compare_outputs(config: Dict[str, Any]) -> None:
@@ -68,76 +76,42 @@ def compare_outputs(config: Dict[str, Any]) -> None:
 
 
 def run(config_file: str) -> None:
-    # Load configuration from JSON file
     with open(config_file) as f:
         config = json.load(f)
         verify_config(config)
 
-    # Create topology
     topo = ClientServerTopo(config)
-    topo_actions = topo.actions
     net = Mininet(topo=topo)
     net.start()
-    handle_actions(net, topo_actions)
+    handle_actions(net, config)
 
-    compare_outputs(config)
+    CLI(net)
 
     net.stop()
 
 
-def handle_actions(net: Mininet, actions=Dict) -> None:  # type: ignore
-    venv_command = "source .venv/bin/activate && "
-
-    # Start server
+def handle_actions(net: Mininet, config) -> None:  # type: ignore
     server = net.get("h1")
-    server_command = actions[server.name]
-    full_server_command = f'bash -c "{venv_command}{server_command}"'
-    print(f"Starting server [{server}] with command: [{full_server_command}]")
-    server_process = server.popen(
-        full_server_command, stdout=sys.stdout, stderr=sys.stderr, shell=True
-    )
+    server_command = f'xterm -hold -e "PYTHONPATH=src python3 ./src/start_server.py -v -H 10.0.0.1 -p {SERVER_PORT} -s {config["server_path"]} -r {config["recovery_protocol"]}" &'
+    server.cmd(server_command)
 
-    # Start clients
-    client_processes = []
-    for client in net.hosts[1:]:
-        time.sleep(random.uniform(0, 2))
-        client_command = actions[client.name]
-        full_client_command = f'bash -c "{venv_command}{client_command}"'
-        print(f"Starting client [{client}] with command: [{full_client_command}]")
-        client_process = client.popen(
-            full_client_command, stdout=sys.stdout, stderr=sys.stderr, shell=True
-        )
-        client_processes.append(client_process)
+    time.sleep(0.5)
 
-    # Start CLI
-    CLI(net)
-
-    # Terminate all processes after CLI exits
-    server_process.terminate()
-    for client_process in client_processes:
-        client_process.terminate()
+    for i, client in enumerate(config["clients"], start=2):
+        client_command = f'xterm -hold -e "PYTHONPATH=src python3 ./src/{client["action"]}.py -v -H 10.0.0.1 -p {SERVER_PORT} -n {client["file"]} -d {client["path"]} -r {config["recovery_protocol"]}" &'
+        client_host = net.get(f"h{i}")
+        client_host.cmd(client_command)
 
 
 def verify_config(config: Dict[str, Any]) -> None:
-    required_fields = ["clients", "loss_percentage", "recovery_protocol", "server_path"]
-    required_client_fields = ["action", "file", "path"]
-    for field in required_fields:
-        if field not in config:
-            raise ValueError(f"Missing required field: {field}")
-
-    if not isinstance(config.get("clients"), list):
-        raise ValueError("Clients should be a list")
-
-        # Ensure all clients have required fields
     for client in config["clients"]:
-        if not isinstance(client, dict):
-            raise ValueError("Each client should be a dictionary")
-        if not all(key in client for key in required_client_fields):
-            raise ValueError(
-                "Each client must have 'action', 'file', and 'path' fields"
-            )
         if not os.path.exists(client["path"]):
-            raise ValueError(f"Path does not exist for client: {client['path']}")
+            if client["action"] == "upload":
+                create_dirs_and_files(client["path"], client["file"], file_size_kb=10)
+            elif client["action"] == "download":
+                create_dirs_and_files(
+                    config["server_path"], client["file"], file_size_kb=10
+                )
 
 
 if __name__ == "__main__":
